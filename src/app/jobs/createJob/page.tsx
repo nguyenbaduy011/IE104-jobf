a8fbf88f66bab8454a69e7a63505466816ad29aa
nguyenbaduy011 <nguyenbaduy011@gmail.com>
2024-12-15 01:23:21 +0700

    feat: adding creating job form

---
+++ src/app/jobs/createJob/page.tsx
@@ -0,0 +1,10 @@
+import JobInformationForm from "@/app/jobs/createJob/jobInformationForm";
+
+
+export default function CreateJob() {
+  return (
+    <div>
+      <JobInformationForm />
+    </div>
+  );
+}
