d463de659dfde6456d3f11684040d936a0f48273
nguyenbaduy011 <nguyenbaduy011@gmail.com>
2024-12-17 20:22:07 +0700

    feat: adding support for inferring additional user fields with plugin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,7 @@
+import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import { auth } from "@/lib/auth";
 export const client = createAuthClient({
+  plugins: [inferAdditionalFields<typeof auth>()],
   baseURL: "http://localhost:3000",
 });
