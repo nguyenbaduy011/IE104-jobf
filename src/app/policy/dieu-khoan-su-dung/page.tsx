b55eabb0715a143956640a51562d2a0a1afa7345
giakhang0812 <phamnguyengiakhang14@gmail.com>
2024-12-22 00:33:56 +0700

    feat: updating policy

---
+++ src/app/policy/dieu-khoan-su-dung/page.tsx
@@ -0,0 +1,216 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <section className="py-20 px-10 bg-gray-100">
+      <div className="max-w-4xl mx-auto bg-white p-10 shadow-lg rounded-lg">
+        <p className="text-lg mb-2 italic text-gray-400">
+          Ngày cập nhật: 30/9/2024
+        </p>
+        <p className="text-lg mb-2 italic text-gray-400">
+          Ngày xuất bản: 02/03/2021
+        </p>
+        <h1 className="text-4xl font-bold py-5 text-red-500">
+          Thoả Thuận Sử Dụng
+        </h1>
+        <p className="text-lg italic flex justify-end">
+          Ngày có hiệu lực: Ngày 12 tháng 7 năm 2023
+        </p>
+        <ol>
+          <li className="text-lg list-decimal list-inside mb-2 font-bold">
+            Giới thiệu
+          </li>
+          <ol>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Chào mừng bạn đến với JobF. Bạn sẽ tìm thấy thông tin liên hệ
+              trong phần{" "}
+              <a
+                href="http://localhost:3000/about"
+                className="text-blue-500 hover:text-blue"
+              >
+                “Về chúng tôi.”
+              </a>
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Bất kỳ thay đổi nào JobF thực hiện đối với Điều khoản & Điều kiện
+              trong tương lai sẽ được đăng trên trang này. Chúng tôi khuyến
+              khích bạn kiểm tra ngày hiệu lực của chính sách này mỗi khi truy
+              cập trang web để cập nhật bất kì thay đổi nào từ phía JobF. Chúng
+              tôi cũng sẽ thông báo cho bạn qua email hoặc tại trang web khi có
+              bất kỳ thay đổi nào trong chính sách của mình.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Việc bạn sử dụng trang web này đồng nghĩa bạn đã đồng ý đối với
+              các điều khoản và điều kiện, tạo thành một hợp đồng giữa bạn và
+              chúng tôi cho việc quản lý và sử dụng. Nếu bạn là người dùng, các
+              quyền theo luật định của bạn sẽ không bị ảnh hưởng bởi thỏa thuận
+              này.
+            </li>
+          </ol>
+          <li className="text-lg list-decimal list-inside mb-2 font-bold">
+            Quyền sở hữu trí tuệ
+          </li>
+          <ol>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Nội dung trang web của chúng tôi bao gồm tất cả thông tin, phần
+              mềm, dữ liệu, văn bản, hình ảnh, âm thanh và video được bảo vệ bởi
+              việc đăng ký bản quyền, thương hiệu, nhãn hiệu dịch vụ, bằng sáng
+              chế hoặc các quyền sở hữu khác.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Ngoại trừ trường hợp cần thiết và được sử dụng trong chừng mực thì
+              nội dung của trang web không được lấy, hiển thị, sửa đổi, sao
+              chép, in, bán, tải về, cho thuê, thiết kế ngược lại hoặc truyền đi
+              dưới bất kỳ hình thức nào mà không có sự đồng ý trước bằng văn bản
+              của chúng tôi.
+            </li>
+          </ol>
+          <li className="text-lg list-decimal list-inside mb-2 font-bold">
+            Bảo vệ dữ liệu
+          </li>
+          <ol>
+            <li className="text-lg list-inside mb-4 ml-3">
+              Thông tin cá nhân được thu thập từ bạn được quy định theo Chính
+              sách Bảo mật của chúng tôi.
+            </li>
+          </ol>
+          <li className="text-lg list-decimal list-inside mb-2 font-bold">
+            Quyền phủ nhận
+          </li>
+          <ol>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Trên trang web này chúng tôi đưa ra nhiều liên kết đến các trang
+              web khác mà chúng tôi nghĩ rằng bạn sẽ quan tâm. Các liên kết được
+              liệt kê tự động dựa trên tên miền và / hoặc các từ khoá được sử
+              dụng trong công cụ tìm kiếm trên trang của chúng tôi và / hoặc các
+              tiêu chí khác. Nếu bạn có bất kỳ mối quan tâm nào về bất kỳ nội
+              dung nào trên trang web này, vui lòng liên hệ với chúng tôi, chúng
+              tôi sẽ cố gắng hết sức để giải quyết.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Chúng tôi không kiểm soát các trang web của bên thứ ba được liệt
+              kê trên trang web này và không xác nhận họ. Bạn đồng ý rằng chúng
+              tôi không chịu trách nhiệm về tính sẵn có hoặc nội dung của hàng
+              hoá hoặc dịch vụ có sẵn trên các trang web đó. Bạn sẽ tự chịu rủi
+              ro nếu truy cập các những trang web này.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Trang web này được cung cấp “như hiện tại”. Chúng tôi không đảm
+              bảo dịch vụ không bị gián đoạn hoặc không có lỗi. Chúng tôi có
+              quyền tạm ngưng hoạt động trang web mà không cần thông báo để phục
+              vụ việc sửa chữa, bảo trì hoặc vì các lý do kỹ thuật khác.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Chúng tôi luôn nỗ lực về mặt thương mại để đảm bảo an ninh cho
+              dịch vụ mà mình cung cấp. Tuy nhiên, chúng tôi không đảm bảo rằng
+              trang web này sẽ hoạt động hoàn toàn hoặc một phần trên bất kỳ
+              thiết bị máy tính cụ thể nào hoặc trong bất kỳ phần mềm cụ thể
+              nào, cũng như không đảm bảo không có virus hoặc bất cứ thứ gì khác
+              có thể gây hại đến bất kỳ công nghệ nào.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Mặc dù chúng tôi luôn có những nỗ lực hợp lý về mặt thương mại để
+              đảm bảo tính chính xác của các nội dung được đăng trên trang web
+              của mình, nhưng chúng tôi không đảm bảo tính chính xác của những
+              nội dung đó.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Chúng tôi luôn có những nỗ lực hợp lý về mặt thương mại để đưa Đơn
+              ứng tuyển của người dùng đến với khách hàng. Tuy nhiên, vì JobF
+              chỉ là một nền tảng nên chúng tôi không chịu trách nhiệm về bất kỳ
+              giao tiếp nào giữa người dùng và khách hàng như việc sắp xếp cuộc
+              phỏng vấn.{" "}
+            </li>
+          </ol>
+          <li className="text-lg list-decimal list-inside mb-2 font-bold">
+            Trách nhiệm
+          </li>
+          <ol>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Phần này (tức là Mục 5. Trách nhiệm) (và bất kỳ điều khoản nào
+              khác loại trừ hoặc giới hạn trách nhiệm của chúng tôi) áp dụng cho
+              các giám đốc, cán bộ, nhân viên, nhà thầu phụ, đại lý và các công
+              ty liên kết cũng như cho chúng tôi. Phần này được áp dụng trong
+              phạm vi tối đa cho phép theo luật pháp hiện hành.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Không có nội dung nào trong thỏa thuận này giới hạn hoặc loại trừ
+              trách nhiệm pháp lý của chúng tôi đối với sự sơ suất gây ra tử
+              vong hoặc thương tích cá nhân, hoặc đối với hành vi xuyên tạc gian
+              lận, hoặc đối với bất kỳ điều gì có thể không được loại trừ hoặc
+              giới hạn về mặt pháp lý.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Chúng tôi loại trừ mọi trách nhiệm pháp lý dưới bất kỳ hình thức
+              nào (kể cả do sơ suất) hoặc bất kỳ trường hợp nào khác liên quan
+              đến trang web này (bao gồm nhưng không giới hạn việc sử dụng trang
+              web, thông tin trên trang web hoặc được truyền tải liên quan đến
+              trang web hoặc bất kỳ vi phạm về an ninh hoặc không có khả năng
+              truy cập trang web) hoặc liên quan đến trang web của bên thứ ba
+              được liên kết từ trang web này.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Không làm ảnh hưởng đến điều trước, trong bất kỳ trường hợp nào
+              (kể cả do sơ suất của chúng tôi) chúng tôi sẽ không chịu trách
+              nhiệm về bất kỳ:
+            </li>
+            <ul className="list-disc list-inside ml-6">
+              <li className="text-lg">
+                Tổn thất kinh tế (bao gồm nhưng không giới hạn việc mất thu
+                nhập, lợi nhuận, hợp đồng, kinh doanh hoặc khoản tiết kiệm dự
+                kiến);
+              </li>
+              <li className="text-lg mb-3">Mất uy tín hoặc danh tiếng;</li>
+              <li className="text-lg mb-3">
+                Tổn thất đặc biệt, gián tiếp hoặc do hậu quả;
+              </li>
+              <li className="text-lg mb-3">
+                Thiệt hại hoặc mất dữ liệu (ngay cả khi chúng tôi đã được thông
+                báo về khả năng xảy ra những thiệt hại đó) đối với trang web
+                này.
+              </li>
+            </ul>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Trong phạm vi pháp luật cho phép, chúng tôi loại trừ tất cả các
+              điều kiện, điều khoản, sự đại diện và bảo đảm, cho dù được quy
+              định bởi pháp luật hoặc các hình thức khác, không được công bố rõ
+              trong thỏa thuận này bao gồm nhưng không giới hạn, các bảo đảm ngụ
+              ý về chất lượng và tính thích đáng cho một mục đích cụ thể.
+            </li>
+          </ol>
+          <li className="text-lg list-decimal list-inside mb-2 font-bold">
+            Điều khoản chung
+          </li>
+          <ol>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Các điều khoản và điều kiện này tạo thành toàn bộ thỏa thuận giữa
+              bạn và chúng tôi liên quan đến việc sử dụng trang web này. Bất kỳ
+              sự không thực hiện hoặc thi hành bất kỳ quyền lợi hoặc điều khoản
+              nào trong những điều khoản và điều kiện này từ phía chúng tôi sẽ
+              không cấu thành sự từ bỏ quyền hoặc quy định đó. Nếu bất kỳ phần
+              nào của thỏa thuận này bị coi là không có hiệu lực vì bất kỳ lý do
+              nào, phần còn lại sẽ tiếp tục có hiệu lực.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Các Điều khoản sẽ được quản lý, giải thích và sẽ có hiệu lực theo
+              quy định của pháp luật Việt Nam; Và sẽ thuộc thẩm quyền độc quyền
+              của tòa án Việt Nam.
+            </li>
+            <li className="text-lg list-decimal list-inside mb-4 ml-3">
+              Dự phòng ưu tiên
+            </li>
+          </ol>
+          <li className="text-lg list-inside mb-4">
+            Giữa công ty chúng tôi và khách hàng, đã được ký kết một Thỏa thuận
+            cho việc cung cấp dịch vụ và trong trường hợp có bất kỳ sự mâu thuẫn
+            nào giữa các điều khoản của Thỏa thuận dịch vụ và Chính sách Bảo mật
+            hoặc Điều khoản & Điều kiện, thì các điều khoản của Thỏa thuận dịch
+            vụ sẽ được ưu tiên áp dụng. Đối với các vấn đề không được quy định
+            trong Thỏa thuận dịch vụ, thì sẽ áp dụng các quy định của Chính sách
+            Bảo mật và Điều khoản & Điều kiện.
+          </li>
+        </ol>
+      </div>
+    </section>
+  );
+}
