a42d084f0655b8679f1487ad6a2f0101d899b7ea
nguyenbaduy011 <nguyenbaduy011@gmail.com>
2024-12-24 06:48:59 +0700

    fix: fixing deploy URL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,5 +3,4 @@ import { createAuthClient } from "better-auth/react";
 import { auth } from "@/lib/auth";
 export const client = createAuthClient({
   plugins: [inferAdditionalFields<typeof auth>()],
-  baseURL: "http://localhost:3000",
 });
